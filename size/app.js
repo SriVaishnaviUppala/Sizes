02886acceb16d3d3e7f8f90011f9cb98084ec8d9
SriVaishnaviUppala <93321676+SriVaishnaviUppala@users.noreply.github.com>
2023-11-20 15:05:44 +0530

    Add files via upload

---
+++ size/app.js
@@ -0,0 +1,12 @@
+function myfun()
+{
+	document.getElementById("heading").style="font-size:20px;"
+}
+function myfun1(size)
+{
+	document.getElementById("heading").style="font-size:50px;"
+}
+function myfun2(size)
+{
+	document.getElementById("heading").style="font-size:90px;"
+}
